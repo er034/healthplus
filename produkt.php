20bc099392f6eebbbd67cff59eb0a5cc86eeffc4
ml109 <ml109@hdm-stuttgart.de>
2018-01-24 17:24:51 +0100

    Startseite Setup 01

---
+++ produkt.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no ">
+    <meta name="description" content="Onlineshop für Nahrungsergänzungsmittel">
+    <!-- Beschreibung unserer Webseite -->
+    <meta name="keywords" content="Nahrungsergänzung, Online Shop , Fitness , Sport">
+    <!-- Suchbbegriffe für die Suchmaschine -->
+    <meta name="author" content="Eileen Röhm, Marc Lunardi, Cedric Tippl">
+    <!-- Authoren der Webseite -->
+    <title>HEALTH PLUS - ENJOY THE DIFFERENCE</title>
+    <link rel= "stylesheet" href="style_produkt.css" type="text/css">
+</head>
+<div>
+    <header>
+        <div class="container">
+            <div id="logo">
+                <a href="index.html"><img src="bilder/logo.svg" alt="Bild" /></a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="produkte.php">PRODUKTE</a></li>
+                    <li><a href="angebote.php">ANGEBOTE</a></li>
+                    <li><a href="blog.php">BLOG</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- Class benutzt man für Objekte die mehrmals in einer Seite vorkommen.
+    ID ist für einzigartige Objekte welche sich nicht wiederholen.-->
+
+    <section id="search">
+        <div class="suche">
+            <form>
+                <input type="produktname" placeholder="Produkt suchen...">
+                <button type="submit" class="button_1">Suchen</button>
+            </form>
+        </div>
+    </section>
+    <!-- Suchfunktion für den Online Shop -->
+    <section id="items">
+        <div class="container">
+        <div class="item">
+            <img src="bilder/item_01.png" width="200" height="300px" border="0" alt="Angebot">
+        </div>
+
+
+    </section>
+    <footer>
+        <p>HealthPlus, Copyright &copy; 2018</p>
+    </footer>
+
+    </body>
+</html>
