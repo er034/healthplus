302dd4aafb18910dc319f744743ec0f70b4f3fc1
ml109 <ml109@hdm-stuttgart.de>
2018-01-24 18:48:02 +0100

    Startseite Setup 01

--- produkt.php
+++ produkt.php
@@ -18,6 +18,7 @@
             <div id="logo">
                 <a href="index.html"><img src="bilder/logo.svg" alt="Bild" /></a>
             </div>
+
             <nav>
                 <ul>
                     <li><a href="produkte.php">PRODUKTE</a></li>
@@ -25,28 +26,41 @@
                     <li><a href="blog.php">BLOG</a></li>
                 </ul>
             </nav>
+            <!-- <form>
+                <input type="produktname" placeholder="Produkt suchen...">
+                <button type="submit" class="button_1">Suchen</button>
+            </form> -->
         </div>
     </header>
+
     <!-- Class benutzt man für Objekte die mehrmals in einer Seite vorkommen.
     ID ist für einzigartige Objekte welche sich nicht wiederholen.-->
 
-    <section id="search">
-        <div class="suche">
-            <form>
-                <input type="produktname" placeholder="Produkt suchen...">
-                <button type="submit" class="button_1">Suchen</button>
+
+    </section>
+    <!-- Suchfunktion für den Online Shop -->
+    <section id="boxes">
+        <div class="container">
+        <div class="box">
+            <img src="bilder/item_01.png" width="300px" height="450px" border="0" alt="Item">
+        </div>
+    </section>
+
+    <section id="select">
+        <div class="container">
+            <form action="/....">
+                <select name="Wähle Deine Größe">
+                    <option value="300g">Pure Oats (300g)</option>
+                    <option value="600g">Pure Oats (600g)</option>
+                    <option value="1000g">Pure Oats (10000g)</option>
+                </select>
+                <input type="submit" value="JETZT AUSWÄHLEN">
             </form>
         </div>
     </section>
     <!-- Suchfunktion für den Online Shop -->
-    <section id="items">
-        <div class="container">
-        <div class="item">
-            <img src="bilder/item_01.png" width="200" height="300px" border="0" alt="Angebot">
-        </div>
 
 
-    </section>
     <footer>
         <p>HealthPlus, Copyright &copy; 2018</p>
     </footer>
