97dac2e520c1ec75848a92c476ebfd59481b44aa
ml109 <ml109@hdm-stuttgart.de>
2018-01-27 19:15:13 +0100

    abmelden + banner

--- abmelden.php
+++ abmelden.php
@@ -1,3 +1,34 @@
 <html>
-<h1>Danke du spast</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no ">
+    <meta name="description" content="Onlineshop für Nahrungsergänzungsmittel">
+    <!-- Beschreibung unserer Webseite -->
+    <meta name="keywords" content="Nahrungsergänzung, Online Shop , Fitness , Sport">
+    <!-- Suchbbegriffe für die Suchmaschine -->
+    <meta name="author" content="Eileen Röhm, Marc Lunardi, Cedric Tippl">
+    <!-- Authoren der Webseite -->
+    <title>HEALTH PLUS - ENJOY THE DIFFERENCE</title>
+    <link rel= "stylesheet" href="style_abmelden.css" type="text/css">
+</head>
+</section>
+<header>
+    <div class="container">
+        <div id="logo">
+            <a href="index.html"><img src="bilder/logo.svg" alt="Bild" /></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="produkte.php">PRODUKTE</a></li>
+                <li><a href="angebote.php">ANGEBOTE</a></li>
+                <li><a href="blog.php">BLOG</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<section id="abgemeldet">
+<div>
+<h1>BIS ZUM NÄCHTSEN MAL!</h1>
+    <a href="index.php">ZURÜCK ZUM SHOP</a>'
+</section>
 </html>
