8a8dcf66ce2ff80e2206a88fec8af3d68d79acd6
ml109 <ml109@hdm-stuttgart.de>
2018-01-30 11:25:40 +0100

    DBConfig

--- dbConfig.php
+++ dbConfig.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cedric
- * Date: 30.01.18
- * Time: 11:18
- */
+//$db = new mysqli('localhost','er034','Ohjiwaux4a','u-er034');
+$server = 'localhost';
+$username = 'er034';
+$password = 'Ohjiwaux4a';
+$dbname = 'u-er034';
+
+$conn = mysqli_connect($server,$username,$password,$dbname);
+if($db->connect_error):
+    echo $db->connect_error;
+endif;
+?>
+
+
