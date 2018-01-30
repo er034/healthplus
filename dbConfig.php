30c9472fc83063c98b29ea3379b99f4dd8e2e178
clain <cedric_tippl@web.de>
2018-01-30 11:29:50 +0100

    Warenkorb

--- dbConfig.php
+++ dbConfig.php
@@ -11,4 +11,16 @@ if($db->connect_error):
 endif;
 ?>
 
+<?php
+//DB details
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'codexworld';
 
+//Create connection and select DB
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Unable to connect database: " . $db->connect_error);
+}
