6615a232ff137faede26e9b335dc6affc2b448a4
ml109 <ml109@hdm-stuttgart.de>
2018-02-04 21:06:31 +0100

    Neues Bild Slider 04

--- index.php
+++ index.php
@@ -83,11 +83,11 @@
         </div>
         <a class="carousel-control-prev" href="#slider_home" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+            <span class="sr-only">Zurück</span>
         </a>
         <a class="carousel-control-next" href="#slider_home" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+            <span class="sr-only">Weiter</span>
         </a>
     </div>
 
