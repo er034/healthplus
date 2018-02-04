132e99940817ca68e14d71bfb0035a20c9736759
ml109 <ml109@hdm-stuttgart.de>
2018-02-04 22:08:57 +0100

    balken png

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style_index.css">
 
-    <title>HEALTH PLUS </title>
+    <title>HEALTH PLUS!</title>
 </head>
 
 <body>
