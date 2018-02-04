4ab4bd2df35d3c7ecb2829730654ceb4e0c733df
eileenroehm <L.08061994.m>
2018-02-05 00:36:00 +0100

    Logo Health Plus angepasst

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light ">
     <a class="navbar-brand" href="index.php">
-        <img src="bilder/logo.png" width="300" height="39" alt="HealthPlus">
+        <img src="bilder/logo.png" width="250" height="33" alt="HealthPlus">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
