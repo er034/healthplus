c64b15615321fddf66193fb3f1d188dddbbea541
ml109 <ml109@hdm-stuttgart.de>
2018-02-04 17:34:33 +0100

    Index + Navbar

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style_index.css">
 
     <title>Hello, world!</title>
@@ -41,10 +38,10 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="angebote" href="angebote.php">ANGEBOTE</a>
+                <a class="nav-link" id="angebote" href="#">ANGEBOTE</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="blog" href="blog.php">BLOG</a>
+                <a class="nav-link" id="blog" href="#">BLOG</a>
             </li>
         </ul>
         <div class="form-inline my-2 " href="warenkorb_uebersicht.php">
@@ -55,11 +52,17 @@
         </div>
     </div>
 </nav>
-
+<div class="container-fluid">
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="bootstrap-4.0.0-dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 <footer>
-    <nav class="footer footernav-expand-lg footernav-light form-inline">
+    <nav class="footernav footernav-expand-lg footernav-light form-inline">
         <div class="footer mx-auto text-center " id="footeritem">
             <a href="impressum.php">IMPRESSUM</a>
             <a href="datenschutz.php">DATENSCHUTZ</a>
