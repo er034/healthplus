68dd200680e4cfd89972a1e222e5cc520cbcc8d2
clain <cedric_tippl@web.de>
2018-02-05 22:32:43 +0100

    Header richtig

--- anmelden.php
+++ anmelden.php
@@ -8,7 +8,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style_index.css">
-    <link rel="stylesheet" href="style_login.css"
+    <link rel="stylesheet" href="style_login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Titel eines Dokumentes -->
     <title>HEALTH PLUS | Home</title>
 </head>
@@ -94,13 +95,6 @@
     </p>
 </form>
 <div class="col-xs-12" style="height:160px;"></div>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-</html>
 
 
 <footer>
