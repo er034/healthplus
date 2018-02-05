6ac83588cfcc9bdba246bd5b9b33ab4b640bbf8f
eileenroehm <L.08061994.m>
2018-02-05 01:36:56 +0100

    Slide Show

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto ml-4">
             <li class="nav-item">
                 <a class="nav-link" id="blog" href="uebersicht.php">PRODUKTE</a>
             </li>
