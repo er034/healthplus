4623e024bb1608d334dfbd79d6c45d760a4641e4
clain <cedric_tippl@web.de>
2018-02-05 22:30:26 +0100

    Header adjustment

--- registrieren.php
+++ registrieren.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style_index.css">
-    <link rel="stylesheet" href="style_login.css"
+    <link rel="stylesheet" href="style_login.css">
     <!-- Titel eines Dokumentes -->
     <title>HEALTH PLUS | Home</title>
 </head>
@@ -126,14 +126,6 @@
         Schon ein Mitglied?<a href="anmelden.php"> Jetzt einloggen</a>
     </p>
 </form>
-
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-</html>
 
 
 <footer>
--- sales_02.php
+++ sales_02.php
@@ -11,6 +11,7 @@ include 'db.php';
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style_index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Titel eines Dokumentes -->
     <title>HEALTH PLUS | Protein Cookies</title>
 </head>
@@ -118,14 +119,6 @@ include 'db.php';
     </div></div>
 
 <div class="col-xs-12" style="height:130px;"></div>
-
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-</html>
 
 
 <footer>
