2fa2da2f656889d429abf4dba6984eec18f908a1
ml109 <ml109@hdm-stuttgart.de>
2018-02-05 18:06:27 +0100

    Index (Mit Login Welcome) + Anmelden/Registrieren

--- kontakt.php
+++ kontakt.php
@@ -58,13 +58,33 @@
     </div>
 </nav>
 
-
 <div class="page-header mt-5 text-center">
     <h5>KONTAKT</h5>
 </div>
 <div align="center">
     <img src="bilder/balken.png" alt="Balken-Icon">
 </div>
+
+<div class="container text-center">
+    <meta charset="UTF-8">
+    <title>HEALTH PLUS | KONTAKT</title>
+    <form method="post" action="kontaktfunktion.php">
+        <label for="Name"><b>Name:</b></label><br>
+        <input type="text" id="Name" name="Name"><br><br>
+
+        <label for="Email"><b>E-Mail:</b></label><br>
+        <input type="text" id="Email" name="Email"><br><br>
+
+        <label for="Betreff"><b>Betreff:</b></label><br>
+        <input type="text" id="Betreff" name="Betreff"><br><br>
+
+        <label for="Nachricht"><b>Nachricht:</b></label><br>
+        <textarea id="Nachricht" name="Nachricht" rows="10" cols="50"></textarea> <br><br>
+
+        <input type="submit" name="submit">
+    </form>
+</div>
+</html>
 
 
 
