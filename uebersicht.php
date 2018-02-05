a8deab860a11d1cee0f6ae37e55fdba69b6f026c
ml109 <ml109@hdm-stuttgart.de>
2018-02-05 02:03:15 +0100

    Neue Produkte

--- uebersicht.php
+++ uebersicht.php
@@ -18,82 +18,58 @@ include 'db.php';
 </head>
 
 <body>
-
 <nav class="navbar navbar-expand-lg navbar-light ">
-    <a class="navbar-brand" href="index.php">
-        <img src="bilder/logo.png" width="300" height="39" alt="HealthPlus">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<a class="navbar-brand" href="index.php">
+    <img src="bilder/logo.png" width="250" height="33" alt="HealthPlus">
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    PRODUKTE
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Produkt 1</a>
-                    <a class="dropdown-item" href="#">Produkt 2</a>
-                    <a class="dropdown-item" href="#">Produkt 3</a>
-                    <a class="dropdown-item" href="#">Produkt 4</a>
-                    <a class="dropdown-item" href="#">Produkt 5</a>
-                    <a class="dropdown-item" href="#">Produkt 6</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    ANGEBOTE
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="angebote_abnehmpaket.php">Abnehm Paket</a>
-                    <a class="dropdown-item" href="angebote_veganpaket.php">Vegan Paket</a>
-                    <a class="dropdown-item" href="angebote_aufbaupaket.php">Aufbau Paket</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    SALES
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="sales_proteinwaffeln.php">Protein Waffeln</a>
-                    <a class="dropdown-item" href="sales_proteincookies.php">Protein Cookies</a>
-                    <a class="dropdown-item" href="sales_proteinpudding.php">Protein Pudding</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="blog" href="blog.php">BLOG</a>
-            </li>
-        </ul>
-        <div class="form-inline my-2 " >
-        <form action="suche.php" method="POST">
-            <input type="text" name="search" placeholder="Suche ein Produkt...">
-            <button type="submit" name="submit-search">Suchen</button>
-        </form>
-        </div>
-        <div class="form-inline my-2 my-lg-1">
-            <a href="warenkorb_uebersicht.php"> <img src="bilder/einkaufswagen_icon.png" width="40" height="40" alt="Einkaufswagen"></a>
-        </div>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ml-4">
+        <li class="nav-item">
+            <a class="nav-link" id="blog" href="uebersicht.php">PRODUKTE</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ANGEBOTE</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="angebote_abnehmpaket.php">Abnehm Paket</a>
+                <a class="dropdown-item" href="angebote_veganpaket.php">Vegan Paket</a>
+                <a class="dropdown-item" href="angebote_aufbaupaket.php">Aufbau Paket</a>
+            </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SALES</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="sales_proteinwaffeln.php">Protein Waffeln</a>
+                <a class="dropdown-item" href="sales_proteincookies.php">Protein Cookies</a>
+                <a class="dropdown-item" href="sales_proteinpudding.php">Protein Pudding</a>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="blog" href="blog.php">BLOG</a>
+        </li>
+    </ul>
+    <div class="form-inline my-2 ml-3" href="warenkorb_uebersicht.php">
+        <a href="uebersicht.php"> <img src="bilder/suche_icon.png" width="25" height="25" alt="Suche"></a>
     </div>
+    <div class="form-inline my-2 my-lg-1 ml-3">
+        <a href="warenkorb_uebersicht.php"> <img src="bilder/einkaufswagen_icon.png" width="25" height="25" alt="Einkaufswagen"></a>
+    </div>
+</div>
 </nav>
 <div class="mt-5 text-center Oberueberschirft"><h5>ALLE PRODUKTE</h5></div>
 <div align="center">
     <img src="bilder/balken.png">
 </div>
-<div class="container">
+<div class="products">
     <div id="products" class="row list-group">
-        <?php
-        //get rows query
-        $query = $db->query("SELECT * FROM products ORDER BY id DESC LIMIT 10");
-        if($query->num_rows > 0){
-            while($row = $query->fetch_assoc()){
-                ?>
         <div class="row justify-content-center">
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_abnehmpaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_01.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_1.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -105,7 +81,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_veganpaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_03.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_2.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -117,7 +93,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_aufbaupaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_02.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_3.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -128,7 +104,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_aufbaupaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_02.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_4.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -139,7 +115,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_aufbaupaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_02.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_5.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -150,7 +126,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_aufbaupaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_02.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_6.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -161,7 +137,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_aufbaupaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_02.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_7.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -172,7 +148,7 @@ include 'db.php';
             <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
                 <a href="angebote_aufbaupaket.php">
                     <div class='card img-container'>
-                        <img class='card-img-top' width='100%' src="bilder/paket_02.jpg">
+                        <img class='card-img-top' width='100%' src="bilder/produkt_8.jpg">
                         <div class='card-body'>
                             <h6><?php echo $row["name"]; ?></h6>
                             <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
@@ -180,14 +156,51 @@ include 'db.php';
                     </div>
                 </a>
             </div>
-
-
+            <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
+                <a href="angebote_aufbaupaket.php">
+                    <div class='card img-container'>
+                        <img class='card-img-top' width='100%' src="bilder/produkt_9.jpg">
+                        <div class='card-body'>
+                            <h6><?php echo $row["name"]; ?></h6>
+                            <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
+            <a href="angebote_aufbaupaket.php">
+                <div class='card img-container'>
+                    <img class='card-img-top' width='100%' src="bilder/sales_01.jpg">
+                    <div class='card-body'>
+                        <h6><?php echo $row["name"]; ?></h6>
+                        <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
+                    </div>
+                </div>
+            </a>
         </div>
-
-
-    <?php } }else{ ?>
-            <p>Product(s) not found.....</p>
-        <?php } ?>
+        <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
+        <a href="angebote_aufbaupaket.php">
+            <div class='card img-container'>
+                <img class='card-img-top' width='100%' src="bilder/sales_02.jpg">
+                <div class='card-body'>
+                    <h6><?php echo $row["name"]; ?></h6>
+                    <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class='new-release mt-4 col-12 col-sm-6 col-lg-3'>
+    <a href="angebote_aufbaupaket.php">
+        <div class='card img-container'>
+            <img class='card-img-top' width='100%' src="bilder/sales_03.jpg">
+            <div class='card-body'>
+                <h6><?php echo $row["name"]; ?></h6>
+                <p class='text-muted'><?php echo '$'.$row["price"].' EURO'; ?></p>
+            </div>
+        </div>
+    </a>
+</div>
+        </div>
  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -196,7 +209,7 @@ include 'db.php';
 </body>
 
 <footer>
-    <nav class="footernav footernav-expand-lg footernav-light form-inline mt-5">
+    <nav class="footernav footernav-expand-lg footernav-light form-inline mx-auto mt-5">
         <div class="footer mx-auto text-center " id="footeritem">
             <a href="impressum.php">IMPRESSUM</a>
             <a href="datenschutz.php">DATENSCHUTZ</a>
